b130df539e69cd7b94cb9ca56b0af0f1d4520aa5
BlindTerran <hongtingsu@gmail.com>
2025-06-23 20:02:34 +1000

    initial commit

---
+++ src/test/java/com/blindterran/JobApp/SpringBootJobAppPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blindterran.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobAppPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
